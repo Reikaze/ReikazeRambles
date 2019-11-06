772de54427e58438ebabac7e3808f03a7b83019c
Corbin Crutchley <corbin.crutchley.contractor@hilton.com>
2019-11-06 11:40:31 -0800

    Fix color issues

--- src/components/theme-context/theme-context.js
+++ src/components/theme-context/theme-context.js
@@ -3,73 +3,76 @@ import { createContext } from "react";
 //css variable names might need to be changed a bit
 export const darkTheme = {
 	//main styles
- "--darkPrimary":  "#FFFFFF",
-  "--black": "white", //🎵 we're gonna party like it's nine-teen eighty-fourrrrrrr 🎵
-  "--white": "black", // 2 + 2 = 5
-  "--darkGrey": "rgba(255, 255, 255, .64)",
-  "--highImpactBlack": "rgba(255, 255, 255, .87)",
-  "--midImpactBlack": "rgba(255, 255, 255, .64)",
-  "--lowImpactBlack": "rgba(255, 255, 255, .58)",
-  "--backgroundColor": "#214F87", //from tommy's mockup
-  "--cardActiveBackground": "#214F87", //from tommy's mockup
-  "--cardActiveBoxShadow": "0px 2px 4px rgba(0, 0, 0, 0.27), inset 0px 1px 0px #435e75", //close to tommy's mockup but outset color is slightly different
-  "--codeBlockBackground": "#202746",
-  //code styles
-  "--codeBackgroundColor": "#161b1d",
-  "--textColor": "#7ea2b4",
-  "--stringColor": "#2d8f6f",
-  "--keywordColor": "#568c3b",
-  "--operatorColor": "#935c25",
-  "--punctuationColor": "#7ea2b4", //this might change
-  "--constantColor": "#aa05d4", //keeping this the same
-  "--functionColor": "#5357d2", //keeping this the same
-  "--selectionColor": "#7195a8",
-  "--commentColor": "#5a7b8c",
-  "--propColor": "#8a8a0f",
-  "--varColor": "#257fad",
-  "--selectorColor": "#6b6bb8",
-  "--urlColor": "#2d8f6f",
-  "--insertedUnderlineColor": "#ebf8ff",
-  "--highlightColor": "#d22d72",
-  "--lineNumbersColor": "#516d7b",
-  "--lineHighlightColor": "rgba(235, 248, 255, 0.2)",
-  "--lineHighlightFadeColor": "rgba(235, 248, 255, 0)",
+	"--darkPrimary": "#FFFFFF",
+	"--primary": "#FFFFFF",
+	"--black": "white", //🎵 we're gonna party like it's nine-teen eighty-fourrrrrrr 🎵
+	"--white": "black", // 2 + 2 = 5
+	"--darkGrey": "rgba(255, 255, 255, .64)",
+	"--highImpactBlack": "rgba(255, 255, 255, .87)",
+	"--midImpactBlack": "rgba(255, 255, 255, .64)",
+	"--lowImpactBlack": "rgba(255, 255, 255, .58)",
+	"--backgroundColor": "#214F87", //from tommy's mockup
+	"--cardActiveBackground": "#214F87", //from tommy's mockup
+	"--cardActiveBoxShadow":
+		"0px 2px 4px rgba(0, 0, 0, 0.27), inset 0px 1px 0px #435e75", //close to tommy's mockup but outset color is slightly different
+	"--codeBlockBackground": "#202746",
+	//code styles
+	"--codeBackgroundColor": "#161b1d",
+	"--textColor": "#7ea2b4",
+	"--stringColor": "#2d8f6f",
+	"--keywordColor": "#568c3b",
+	"--operatorColor": "#935c25",
+	"--punctuationColor": "#7ea2b4", //this might change
+	"--constantColor": "#aa05d4", //keeping this the same
+	"--functionColor": "#5357d2", //keeping this the same
+	"--selectionColor": "#7195a8",
+	"--commentColor": "#5a7b8c",
+	"--propColor": "#8a8a0f",
+	"--varColor": "#257fad",
+	"--selectorColor": "#6b6bb8",
+	"--urlColor": "#2d8f6f",
+	"--insertedUnderlineColor": "#ebf8ff",
+	"--highlightColor": "#d22d72",
+	"--lineNumbersColor": "#516d7b",
+	"--lineHighlightColor": "rgba(235, 248, 255, 0.2)",
+	"--lineHighlightFadeColor": "rgba(235, 248, 255, 0)"
 };
 
 export const lightTheme = {
 	//main styles
 	"--darkPrimary": "#214F87",
-  "--primary": "#127DB3",
-  "--black": "black",
-  "--white": "white",
-  "--darkGrey": "rgba(0, 0, 0, 0.64)",
-  "--highImpactBlack": "rgba(0, 0, 0, 0.87)",
-  "--midImpactBlack": "rgba(0, 0, 0, 0.64)",
-  "--lowImpactBlack": "rgba(0, 0, 0, 0.58)",
-  "--backgroundColor": "#FFFFFF",
-  "--cardActiveBackground": "#FFFFFF",
-  "--cardActiveBoxShadow": "0px 2px 4px rgba(11, 37, 104, 0.27), inset 0px 1px 0px #FFFFFF",
-  "--codeBlockBackground": "white",
-  //code styles
-  "--codeBackgroundColor": "#fff",
-  "--textColor": "#5e6687",
-  "--stringColor": "#007396",
-  "--keywordColor": "#846c00",
-  "--operatorColor": "#b74c00",
-  "--punctuationColor": "#006fce",
-  "--constantColor": "#aa05d4",
-  "--functionColor": "#5357d2",
-  "--selectionColor": "#dfe2f1",
-  "--commentColor": "#898ea4",
-  "--propColor": "#c08b30",
-  "--varColor": "#3d8fd1",
-  "--selectorColor": "#6679cc",
-  "--urlColor": "#22a9c9",
-  "--insertedUnderlineColor": "#202746",
-  "--highlightColor": "#c94922",
-  "--lineNumbersColor": "#979db4",
-  "--lineHighlightColor": "rgba(107, 115, 148, 0.2)",
-  "--lineHighlightFadeColor": "rgba(107, 115, 148, 0)",
+	"--primary": "#127DB3",
+	"--black": "black",
+	"--white": "white",
+	"--darkGrey": "rgba(0, 0, 0, 0.64)",
+	"--highImpactBlack": "rgba(0, 0, 0, 0.87)",
+	"--midImpactBlack": "rgba(0, 0, 0, 0.64)",
+	"--lowImpactBlack": "rgba(0, 0, 0, 0.58)",
+	"--backgroundColor": "#FFFFFF",
+	"--cardActiveBackground": "#FFFFFF",
+	"--cardActiveBoxShadow":
+		"0px 2px 4px rgba(11, 37, 104, 0.27), inset 0px 1px 0px #FFFFFF",
+	"--codeBlockBackground": "white",
+	//code styles
+	"--codeBackgroundColor": "#fff",
+	"--textColor": "#5e6687",
+	"--stringColor": "#007396",
+	"--keywordColor": "#846c00",
+	"--operatorColor": "#b74c00",
+	"--punctuationColor": "#006fce",
+	"--constantColor": "#aa05d4",
+	"--functionColor": "#5357d2",
+	"--selectionColor": "#dfe2f1",
+	"--commentColor": "#898ea4",
+	"--propColor": "#c08b30",
+	"--varColor": "#3d8fd1",
+	"--selectorColor": "#6679cc",
+	"--urlColor": "#22a9c9",
+	"--insertedUnderlineColor": "#202746",
+	"--highlightColor": "#c94922",
+	"--lineNumbersColor": "#979db4",
+	"--lineHighlightColor": "rgba(107, 115, 148, 0.2)",
+	"--lineHighlightFadeColor": "rgba(107, 115, 148, 0)"
 };
 
 export function setThemeColorsToVars(themeName) {
